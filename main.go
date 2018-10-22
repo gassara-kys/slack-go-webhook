ef02fea49c174d3f232d327fb9f6924730d222a9
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2018-10-22 11:56:35 +0900

    change pointer param

--- main.go
+++ main.go
@@ -6,34 +6,37 @@ import (
 	"github.com/parnurzeal/gorequest"
 )
 
+// Field in attachment
 type Field struct {
 	Title string `json:"title"`
 	Value string `json:"value"`
 	Short bool   `json:"short"`
 }
 
+// Attachment in message payload
 type Attachment struct {
-	Fallback   *string   `json:"fallback"`
-	Color      *string   `json:"color"`
-	PreText    *string   `json:"pretext"`
-	AuthorName *string   `json:"author_name"`
-	AuthorLink *string   `json:"author_link"`
-	AuthorIcon *string   `json:"author_icon"`
-	Title      *string   `json:"title"`
-	TitleLink  *string   `json:"title_link"`
-	Text       *string   `json:"text"`
-	ImageUrl   *string   `json:"image_url"`
-	Fields     []*Field  `json:"fields"`
-	Footer     *string   `json:"footer"`
-	FooterIcon *string   `json:"footer_icon"`
-	Timestamp  *int64    `json:"ts"`
-	MarkdownIn *[]string `json:"mrkdwn_in"`
+	Fallback   string   `json:"fallback"`
+	Color      string   `json:"color"`
+	PreText    string   `json:"pretext"`
+	AuthorName string   `json:"author_name"`
+	AuthorLink string   `json:"author_link"`
+	AuthorIcon string   `json:"author_icon"`
+	Title      string   `json:"title"`
+	TitleLink  string   `json:"title_link"`
+	Text       string   `json:"text"`
+	ImageURL   string   `json:"image_url"`
+	Fields     []Field  `json:"fields"`
+	Footer     string   `json:"footer"`
+	FooterIcon string   `json:"footer_icon"`
+	Timestamp  int64    `json:"ts"`
+	MarkdownIn []string `json:"mrkdwn_in"`
 }
 
+// Payload slack message payload
 type Payload struct {
 	Parse       string       `json:"parse,omitempty"`
 	Username    string       `json:"username,omitempty"`
-	IconUrl     string       `json:"icon_url,omitempty"`
+	IconURL     string       `json:"icon_url,omitempty"`
 	IconEmoji   string       `json:"icon_emoji,omitempty"`
 	Channel     string       `json:"channel,omitempty"`
 	Text        string       `json:"text,omitempty"`
@@ -44,19 +47,15 @@ type Payload struct {
 	Markdown    bool         `json:"mrkdwn,omitempty"`
 }
 
-func (attachment *Attachment) AddField(field Field) *Attachment {
-	attachment.Fields = append(attachment.Fields, &field)
-	return attachment
-}
-
 func redirectPolicyFunc(req gorequest.Request, via []gorequest.Request) error {
 	return fmt.Errorf("Incorrect token (redirection)")
 }
 
-func Send(webhookUrl string, proxy string, payload Payload) []error {
+// Send post message via proxy
+func Send(webhookURL string, proxy string, payload Payload) []error {
 	request := gorequest.New().Proxy(proxy)
 	resp, _, err := request.
-		Post(webhookUrl).
+		Post(webhookURL).
 		RedirectPolicy(redirectPolicyFunc).
 		Send(payload).
 		End()
